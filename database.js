d2b44d3922270b2a9589e1b8bfc6042b74b02d9d
tadeopc98 <124084565+tadeopc98@users.noreply.github.com>
2023-08-01 19:14:14 -0600

    Update database.js

--- database.js
+++ database.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2');
 function createConnection() {
   const connection = mysql.createConnection({
     host: 'db-mysql-nyc1-34916-do-user-14456800-0.b.db.ondigitalocean.com',
-    user: 'doadmin',
-    password: 'AVNS_EykNvKZvK_acs6OX1VK',
+    user: 'tpc98',
+    password: 'AVNS_LJsJZyFu7aEv4QmdhQS',
     database: 'gasunion',
     port: 25060,
     
